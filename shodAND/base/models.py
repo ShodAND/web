0d9aa3bc18f4abaa7a2f72ad13a6ecaba1205d6b
Xavi Torelló <info@xaviertorello.cat>
2017-11-19 13:07:51 +0100

    Add Django base script and base DB

--- shodAND/base/models.py
+++ shodAND/base/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Host(models.Model):
     hostname = models.CharField(max_length=200)
+    ip = models.CharField(max_length=200)
     creation_date = models.DateTimeField('date created')
     modification_date = models.DateTimeField('date modified')
