3c9617b51652bcc1a5950987a0c449bb49b03196
Xavi Torelló <info@xaviertorello.cat>
2017-11-19 14:25:22 +0100

    Define the expected string representation for each model

--- shodAND/base/models.py
+++ shodAND/base/models.py
@@ -6,13 +6,23 @@ class Host(models.Model):
     creation_date = models.DateTimeField('date created')
     modification_date = models.DateTimeField('date modified')
 
+    def __str__(self):
+        return f"{self.ip}"
+
 class Port(models.Model):
     port = models.IntegerField()
     creation_date = models.DateTimeField('date created')
     modification_date = models.DateTimeField('date modified')
+
+    def __str__(self):
+        return f"{self.port}"
+
 
 class Scan(models.Model):
     host = models.ForeignKey(Host, on_delete=models.CASCADE)
     ports = models.ManyToManyField(Port)
     creation_date = models.DateTimeField('date created')
     modification_date = models.DateTimeField('date modified')
+
+    def __str__(self):
+        return f"{self.host} [{self.ports}]"
