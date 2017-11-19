4125dbbb8677ee3c16b05927250a8637262d6573
Xavi Torelló <info@xaviertorello.cat>
2017-11-19 14:04:30 +0100

    Add PortAPI viewset

--- shodAND/base/api.py
+++ shodAND/base/api.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from .models import Host
-from .serializers import HostSerializer
+from .models import Host, Port
+from .serializers import HostSerializer, PortSerializer
 
 class HostAPI(viewsets.ModelViewSet):
     """
@@ -9,3 +9,10 @@ class HostAPI(viewsets.ModelViewSet):
     """
     queryset = Host.objects.all().order_by('-creation_date')
     serializer_class = HostSerializer
+
+class PortAPI(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Port.objects.all().order_by('-creation_date')
+    serializer_class = PortSerializer
