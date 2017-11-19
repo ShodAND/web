3224b585604407d8a21247e46ff28cae64af3957
Xavi Torelló <info@xaviertorello.cat>
2017-11-19 14:04:17 +0100

    Add PortSerializer

--- shodAND/base/serializers.py
+++ shodAND/base/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Host
+from .models import Host, Port
 
 class HostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Host
         fields = ('hostname', 'ip', 'creation_date', 'modification_date')
+
+class PortSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Port
+        fields = ('port', 'creation_date', 'modification_date')
