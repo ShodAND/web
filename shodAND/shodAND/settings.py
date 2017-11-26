be2fd128de5d5227e7dfa523b87ea81276e91ac7
Xavi Torelló <info@xaviertorello.cat>
2017-11-26 17:39:57 +0100

    Testing indigo blue

--- shodAND/shodAND/settings.py
+++ shodAND/shodAND/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.theme.deeppurple',
+    'material.theme.indigo',
     'material',
     'material.admin',
     'django.contrib.admin',
