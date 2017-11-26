679b2c383a548a5b70333fd1fdcf5a67912a3aa6
Xavi Torelló <info@xaviertorello.cat>
2017-11-26 17:39:10 +0100

    Testing green

--- shodAND/shodAND/settings.py
+++ shodAND/shodAND/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.theme.amber',
+    'material.theme.lightgreen',
     'material',
     'material.admin',
     'django.contrib.admin',
