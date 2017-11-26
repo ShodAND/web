0da9999f028815793643fdb326ba8614528aef61
Xavi Torelló <info@xaviertorello.cat>
2017-11-26 17:40:09 +0100

    Testing light blue

--- shodAND/shodAND/settings.py
+++ shodAND/shodAND/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.theme.indigo',
+    'material.theme.lightblue',
     'material',
     'material.admin',
     'django.contrib.admin',
