3fb24e49c7da62cbef6d999121c8620ed0fece63
Xavi Torelló <info@xaviertorello.cat>
2017-12-03 21:04:05 +0100

    Add default ScanProcess model

--- shodAND/flows/models.py
+++ shodAND/flows/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from viewflow.models import Process
 
-# Create your models here.
+AVAILABLE_STATUS = (
+    ('todo', 'To do'),
+    ('pending', 'Pending'),
+    ('wip', 'Work in progress'),
+    ('done', 'Done'),
+)
+
+class ScanProcess(Process):
+    """ Simple scan model """
+    result = models.CharField(max_length=150)
+    status = models.CharField(
+        max_length=20,
+        choices=AVAILABLE_STATUS,
+        default="todo",
+    )
+    error = models.BooleanField(default=False)
+
