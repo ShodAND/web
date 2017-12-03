dd65b0a4a6c2390a9fe162a454cba533292febfb
Xavi Torelló <info@xaviertorello.cat>
2017-12-03 22:06:41 +0100

    Add workflow and frontend urls

--- shodAND/shodAND/urls.py
+++ shodAND/shodAND/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from base import api
+from django.views import generic
+from material.frontend import urls as frontend_urls
 
 urlpatterns = [
     url(r'^api/v1/', include(api.routes.urls)),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^base/', include('base.urls')),
+    url(r'^$', generic.RedirectView.as_view(url='/workflow/', permanent=False)),
+    url(r'', include(frontend_urls)),
 ]
