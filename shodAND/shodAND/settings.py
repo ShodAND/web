96b73d7605e89ce71bf990b19fef4a366af78e8b
Xavi Torelló <info@xaviertorello.cat>
2017-12-04 20:32:56 +0100

    Add CELERY settings

--- shodAND/shodAND/settings.py
+++ shodAND/shodAND/settings.py
@@ -29,6 +29,13 @@ ALLOWED_HOSTS = []
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Andorra'
+
 
 
 # Application definition
