b0f92f3f3ae4d6e989f12a822b71f08cb640d73c
Xavi Torelló <info@xaviertorello.cat>
2017-12-04 21:40:19 +0100

    Try to reach the related scan and try to execute it! //WIP

--- shodAND/flows/tasks.py
+++ shodAND/flows/tasks.py
@@ -8,10 +8,32 @@ import time
 logger = get_task_logger(__name__)
 
 @shared_task
-def perform_scan(host, ports):
-    #assert 
-    new_scan = Scan(host=host, ports=ports)
+def perform_scan(scan_id):
+    time.sleep(2)
+
+    assert scan_id and type(scan_id) == int, "Provided Scan id '{}' is not valid.".format(scan_id) 
+
+    try:
+        new_scan = Scan.objects.get(pk=scan_id)
+    except:
+        assert False, "Provided Scan with id #{} do not exist.".format(scan_id)
+
+    try:
+        # Execute the scan using the shodAND-scanner
+        pass
+    except:
+        new_scan.state = "pending"
+        new_scan.error = True
+        new_scan.save()
+        assert False, "Changes can't be saved for #{}.".format(scan_id)
+
     time.sleep(5)
-    logger.info(host, ports)
-    print("job done! [{} {}]".format(host, ports))
-    return "job done! [{} {}]".format(host, ports)
+    new_scan.state = "done"
+    new_scan.error = False
+    new_scan.result = "Sample result asdkajdasdjasdlj"
+
+    new_scan.save()
+
+    logger.info(scan_id)
+    print("job done! [{} {}]".format(scan_id, "host"))
+    return "job done! [{} {}]".format(scan_id, "host")
